67ad0b463960d59161fde30899a65844ed687287
Numan <numan.a>
2025-07-26 18:47:03 +0530

    Added socket.io integration and events for chat basic functionality

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const cors = require('cors')
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+const { EVENT_JOIN, EVENT_MESSAGE, EVENT_MESSAGE_RECEIVED, EVENT_CHAT } = require('./src/utils/constants.ts');
 
 const app = express();
+app.use(cors());
+
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to LetsConnect Chat-App</h1>');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('Socket connected:', socket.id);
+  socket.on(EVENT_JOIN, (data) => {
+    console.log('User joined:', data);
+    io.emit(EVENT_CHAT, `${data}, Joined Chat.`);
+  });
+  socket.on(EVENT_MESSAGE, (data) => {
+    console.log('Message received:', data);
+    io.emit(EVENT_MESSAGE_RECEIVED, data);
+  });
 });
 
 const port = process.env.PORT || 5000;
---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  EVENT_MESSAGE: 'message',
+  EVENT_MESSAGE_RECEIVED: 'message_received',
+  EVENT_CHAT: 'chat_event',
+  EVENT_JOIN: 'join',
+  EVENT_USER_LEFT: 'userLeft',
+};
